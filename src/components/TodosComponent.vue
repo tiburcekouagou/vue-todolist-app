35895f986041dd6cd4b710bef243c467017d8a03
Tiburce Kouagou <kouagoutiburce@gmail.com>
2024-06-13 19:47:18 +0200

    ajout de types globaux pour l'application

--- src/components/TodosComponent.vue
+++ src/components/TodosComponent.vue
@@ -29,11 +29,12 @@
 </template>
 
 <script setup lang="ts">
+import type { Todo } from '@/@types'
 import TodoFooter from '@/components/TodoFooter.vue'
 import TodoHeader from '@/components/TodoHeader.vue'
 import { ref } from 'vue'
 
-const todos = ref([
+const todos = ref<Todo[]>([
   { id: 'cf972fd6-8ca0-4c38-af79-b726e367d618', title: 'Ma tâche 1', completed: false },
   { id: 'ab81b122-4df5-47a3-982d-cbc928c35a18', title: 'Ma tâche 2', completed: false },
   { id: '8aed622a-90b1-4976-a430-97b6fc74b0a6', title: 'Ma tâche 3', completed: false }
