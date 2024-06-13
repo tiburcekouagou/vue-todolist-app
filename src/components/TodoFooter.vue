fec8e8508916a659d28e4d02f8527b35b6fb2d6f
Tiburce Kouagou <kouagoutiburce@gmail.com>
2024-06-13 18:59:37 +0200

    footer créé

--- src/components/TodoFooter.vue
+++ src/components/TodoFooter.vue
@@ -1,5 +1,21 @@
 <template>
-  <div>footer</div>
+  <footer class="footer">
+    <span class="todo-count"> <strong>10</strong> tâches restantes </span>
+
+    <ul class="filters">
+      <li>
+        <RouterLink to="#">Tous</RouterLink>
+      </li>
+      <li>
+        <RouterLink to="#">En cours</RouterLink>
+      </li>
+      <li>
+        <RouterLink to="#">Terminé</RouterLink>
+      </li>
+    </ul>
+
+    <button class="clear-completed">Sup. tâches terminées</button>
+  </footer>
 </template>
 
 <script setup lang="ts"></script>
