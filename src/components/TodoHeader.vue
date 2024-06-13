2698f7460da69bef893ab168169cae0e88538ee7
Tiburce Kouagou <kouagoutiburce@gmail.com>
2024-06-13 18:50:14 +0200

    header créé

---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <header class="header">
+    <h1>Todos</h1>
+
+    <input
+      type="text"
+      class="new-todo"
+      placeholder="Entrez une tâche"
+      autofocus
+      autocomplete="off"
+    />
+  </header>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
--- src/components/TodosComponent.vue
+++ src/components/TodosComponent.vue
@@ -1,7 +1,17 @@
 <template>
-  <h1>Todos Components</h1>
+  <div>
+    <!-- En-tête (titre, input) -->
+    <TodoHeader />
+    <!-- Liste des tâches -->
+
+    <!-- Filtres -->
+    <TodoFooter />
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import TodoFooter from '@/components/TodoFooter.vue'
+import TodoHeader from '@/components/TodoHeader.vue'
+</script>
 
 <style></style>
