4aa8c7fa34db0a25839ace8fa34e1dea6c4bd580
Tiburce Kouagou <kouagoutiburce@gmail.com>
2024-06-13 19:18:43 +0200

    structure base du TodosComponent créée

--- src/components/TodosComponent.vue
+++ src/components/TodosComponent.vue
@@ -2,10 +2,41 @@
   <div>
     <!-- En-tête (titre, input) -->
     <TodoHeader />
+    <!-- /En-tête (titre, input) -->
+
     <!-- Liste des tâches -->
+    <main class="main">
+      <div class="toggle-all-container">
+        <input type="checkbox" id="toggle-all-input" class="" />
+        <label for="toggle-all-input" class="">Tout afficher</label>
+      </div>
+
+      <ul class="todo-list">
+        <li>
+          <div class="view">
+            <input type="checkbox" id="" class="toggle" />
+            <label for="">Tâche 1</label>
+          </div>
+        </li>
+        <li>
+          <div class="view">
+            <input type="checkbox" id="" class="toggle" />
+            <label for="">Tâche 2</label>
+          </div>
+        </li>
+        <li>
+          <div class="view">
+            <input type="checkbox" id="" class="toggle" />
+            <label for="">Tâche 3</label>
+          </div>
+        </li>
+      </ul>
+    </main>
+    <!-- /Liste des tâches -->
 
     <!-- Filtres -->
     <TodoFooter />
+    <!-- / Filtres -->
   </div>
 </template>
 
